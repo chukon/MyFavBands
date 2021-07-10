d99a0f21ae1298ab4ff4cba7b02bae4a6bcdb11b
Chuck Konkol <ckonkol@gmail.com>
2021-07-10 10:26:00 -0500

    ck

--- MyFavBands/MasterViewController.swift
+++ MyFavBands/MasterViewController.swift
@@ -2,8 +2,8 @@
 //  MasterViewController.swift
 //  MyFavBands
 //
-//  Created by Charles Konkol on 9/15/19.
-//  Copyright © 2019 Charles Konkol. All rights reserved.
+//  Created by Charles Konkol on 7/10/2021.
+//  Copyright © 2021 Charles Konkol. All rights reserved.
 //
 
 import UIKit
